b064bd39dd84bb34ee6f599f38973ff4ca035a5d
niraj-kumar2 <126452817+niraj-kumar2@users.noreply.github.com>
2025-07-23 17:25:22 +0530

    commit test

--- js-backend/app.js
+++ js-backend/app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+app.get("/", (req, res) => {
+    res.send("server running")
+})
+
 app.listen(port, () => {
     console.log("server is running", port)
 })
