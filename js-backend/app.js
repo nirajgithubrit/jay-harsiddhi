c4626630e3388076c32b916b8a9caeb952e02f73
niraj-kumar2 <126452817+niraj-kumar2@users.noreply.github.com>
2025-12-20 12:00:24 +0530

    task-main
    set up for deployment

--- js-backend/app.js
+++ js-backend/app.js
@@ -1,8 +1,9 @@
+const dotenv = require("dotenv")
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const cors = require("cors")
-const port = 3000
+const port = process.env.PORT || 3000
 const authRoute = require("./routes/auth")
 const brandRoute = require("./routes/brand")
 const categoryRoute = require("./routes/category")
@@ -10,8 +11,12 @@ const colorRoute = require("./routes/color")
 const materialRoute = require("./routes/material")
 const customerRoute = require("./routes/customer")
 
+//Allow access to .env file
+dotenv.config()
+
 app.use(cors({
-  origin: '*'
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
@@ -27,7 +32,7 @@ app.use("/material", materialRoute)
 app.use("/customer", customerRoute)
 
 async function connectDb() {
-    await mongoose.connect("mongodb://localhost:27017/", {
+    await mongoose.connect(process.env.MONGO_URI, {
         dbName: "jay-harsiddhi-db"
     })
 
@@ -41,5 +46,3 @@ connectDb().catch((err) => {
 app.listen(port, '0.0.0.0', () => {
     console.log("server is running", port)
 })
-
-
