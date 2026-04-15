8bc6ef9dc1566f721179a1bf3f9b8b467f48c123
niraj-kumar2 <126452817+niraj-kumar2@users.noreply.github.com>
2026-04-15 13:15:27 +0530

    task-main

--- js-backend/app.js
+++ js-backend/app.js
@@ -1,47 +1,54 @@
-const dotenv = require("dotenv")
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const cors = require("cors")
-const port = process.env.PORT || 3000
-const authRoute = require("./routes/auth")
-const brandRoute = require("./routes/brand")
-const categoryRoute = require("./routes/category")
-const colorRoute = require("./routes/color")
-const materialRoute = require("./routes/material")
-const customerRoute = require("./routes/customer")
-const { verifyToken, isAdmin, isSalesPerson } = require("./middleware/auth-middleware")
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const authRoute = require("./routes/auth");
+const brandRoute = require("./routes/brand");
+const categoryRoute = require("./routes/category");
+const colorRoute = require("./routes/color");
+const materialRoute = require("./routes/material");
+const customerRoute = require("./routes/customer");
+const {
+  verifyToken,
+  isAdmin,
+  isSalesPerson,
+} = require("./middleware/auth-middleware");
 
 //Allow access to .env file
-dotenv.config()
+dotenv.config();
 
 mongoose.set("strictQuery", true);
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}))
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("server running")
-})
+  res.send("server running");
+});
 
 app.get("/test-db", async (req, res) => {
   const state = mongoose.connection.readyState;
-  res.json({ mongoState: state }); 
+  res.json({ mongoState: state });
 });
 
-app.use("/auth", authRoute)
-app.use("/brand", verifyToken, isAdmin, brandRoute)
-app.use("/category", verifyToken, isAdmin, categoryRoute)
-app.use("/color", verifyToken, isAdmin, colorRoute)
-app.use("/material", verifyToken, isAdmin, materialRoute)
-app.use("/customer", verifyToken, isSalesPerson, customerRoute)
+app.use("/auth", authRoute);
+app.use("/brand", verifyToken, isAdmin, brandRoute);
+app.use("/category", verifyToken, isAdmin, categoryRoute);
+app.use("/color", verifyToken, isAdmin, colorRoute);
+app.use("/material", verifyToken, isAdmin, materialRoute);
+app.use("/customer", verifyToken, isSalesPerson, customerRoute);
 
 async function connectDb() {
-    try {
+  try {
     await mongoose.connect(process.env.MONGO_URI, {
       dbName: "jay-harsiddhi-db",
     });
@@ -53,9 +60,9 @@ async function connectDb() {
 }
 
 connectDb().catch((err) => {
-    console.log(err);
-})
+  console.log(err);
+});
 
-app.listen(port, '0.0.0.0', () => {
-    console.log("server is running", port)
-})
+app.listen(port, "0.0.0.0", () => {
+  console.log("server is running", port);
+});
--- js-backend/routes/auth.js
+++ js-backend/routes/auth.js
@@ -1,53 +1,52 @@
-const express = require("express")
-const { registerUser, loginUser } = require("../handlers/auth-handler")
-const router = express.Router()
+const express = require("express");
+const { registerUser, loginUser } = require("../handlers/auth-handler");
+const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    let model = req.body
-    if (model.name && model.email && model.password) {
-        await registerUser(model)
-        res.send({
-            message: "User Registered"
-        })
-    } else {
-        res.status(400).json({
-            error: "Please Provide name, email and password"
-        })
-    }
-})
-
-router.post("/login", async (req, res) => {
-    let model = req.body
-    if (model.email && model.password) {
-        const result = await loginUser(model)
-        if (result) {
-            res.send(result)
-        } else {
-            res.status(400).json({
-                error: "Email or password is incorrect"
-            })
-        }
-    } else {
-        res.status(400).json({
-            error: "Please Provide email and password"
-        })
-    }
-})
-
-router.post("/refresh-token", async (req, res) => {
-    const refreshToken = req.body;
-
-    if (!refreshToken) {
-        return res.status(401).json({ error: "Refresh token required" });
-    }
-
-    try {
-        const result = await refreshToken(refreshToken)
-        res.json({ accessToken: result });
-    } catch (err) {
-        return res.status(403).json({ error: "Invalid refresh token" });
-    }
+  let model = req.body;
+  if (model.name && model.email && model.password) {
+    await registerUser(model);
+    res.send({
+      message: "User Registered",
+    });
+  } else {
+    res.status(400).json({
+      error: "Please Provide name, email and password",
+    });
+  }
 });
 
+router.post("/login", async (req, res) => {
+  let model = req.body;
+  if (model.email && model.password) {
+    const result = await loginUser(model);
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(400).json({
+        error: "Email or password is incorrect",
+      });
+    }
+  } else {
+    res.status(400).json({
+      error: "Please Provide email and password",
+    });
+  }
+});
 
-module.exports = router
+router.post("/refresh-token", async (req, res) => {
+  const token = req.body;
+
+  if (!token) {
+    return res.status(401).json({ error: "Refresh token required" });
+  }
+
+  try {
+    const result = await refreshToken(token);
+    res.json({ accessToken: result });
+  } catch (err) {
+    return res.status(403).json({ error: "Invalid refresh token" });
+  }
+});
+
+module.exports = router;
