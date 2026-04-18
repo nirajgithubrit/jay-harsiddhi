1e262ae14716df8b53b0288cd668c1bf5325dd2f
niraj-kumar2 <126452817+niraj-kumar2@users.noreply.github.com>
2026-04-18 12:38:23 +0530

    Update app.js

--- js-backend/app.js
+++ js-backend/app.js
@@ -48,7 +48,7 @@ app.use(
 
 // ✅ 🔥 IMPORTANT: Handle preflight requests
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "https://jayharsiddhi.netlify.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
